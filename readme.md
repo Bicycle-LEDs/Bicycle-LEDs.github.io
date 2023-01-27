cacf8c0ba6e334d565646188a8393ef4c64a4c40
Bajojajo-xD <81306360+LukeMech@users.noreply.github.com>
2023-01-27 19:54:39 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# 🪄 Main project site
+
+## 🚀 Hosted [here](https://modular-lamps.github.io/)
+
+## 🔥 This repository contains:
+- 🪢 Source code for website
+
+## 🛠️ If you are looking for something else:
+- 🏗️ [Firmware repository](https://github.com/Modular-Lamps/firmware/)
+- 🛠️ [Hardware testing files](https://github.com/Modular-Lamps/hardware-tests)
+- 🧬 [Electronics](https://github.com/Modular-Lamps/electronics)
+
+## ⚙️ Updates
+- ⚠️ Critical or bug-fixing updates are provided every week
+- 🌋 New features are added at least once every 3 months
---
+++ scripts/announcment.js
@@ -0,0 +1,21 @@
+var globalAnnouncment = 
+
+//HERE WRITE ANNOUNCMENT
+'<i class="fa-solid fa-bell"></i> Site not fully ready'
+
+
+
+//HERE WRITE ADDITIONAL ANNOUNCMENTS FOR SPECIFIED FILES
+var mainAnnouncment = '',
+    aboutAnnouncment = '',
+    docsAnnouncment = '<i class="fa-solid fa-house-circle-exclamation"></i> Docs under building'
+
+
+var fileName = location.href.split("/").slice(-1), text;
+var announcment = document.getElementById("announcment")
+if(fileName[0] == 'docs.html') text = docsAnnouncment
+else if (fileName[0] == 'about.html') text = aboutAnnouncment
+else if (fileName[0] == 'index.html') text = mainAnnouncment
+text ? (globalAnnouncment ? text = text + '<br />' + globalAnnouncment : text = text) : (globalAnnouncment ? text = globalAnnouncment : text = '')
+
+announcment.innerHTML = text
