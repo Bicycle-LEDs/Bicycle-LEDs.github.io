2f3350e98cbc2835d2f9578ec24d71d249db769b
Bajojajo-xD <81306360+LukeMech@users.noreply.github.com>
2023-01-29 16:21:55 +0100

    ✨ Add styling to menu

--- scripts/docsNavigator.js
+++ scripts/docsNavigator.js
@@ -1,38 +1,62 @@
 var menu = document.getElementById("menu")
+var loading = document.getElementById("loading")
+var main = document.getElementById("mainSection")
+
 var selectedMenu = 0
 var selectedSubMenu = -1
+var subClicked = false
 
-async function buildMenu() {
+async function buildMenu(firstRun = true) {
+
+  if(!firstRun) {
+    loading.style.display = "block"
+    main.style.display = "none"
+  }  
+
   try {
+    menu.innerHTML = ""
     modules = await fetch('https://raw.githubusercontent.com/Bicycle-LEDs/electronics/main/modules.json').then(res => res.json())
-    
-    for (let k = 0; k < modules.length; k++) {
-      if(selectedSubMenu == -1 && selectedMenu == k)  menu.innerHTML = menu.innerHTML + "\n" + '<ul onclick="mainDoc(' + k + ')" class="selected" id="' + modules[k].name + '">' + modules[k].name
-      else menu.innerHTML = menu.innerHTML + "\n" + '<ul onclick="mainDoc(' + k + ')" id="' + modules[k].name + '">' + modules[k].name
 
+    for (let k = 0; k < modules.length; k++) {
       let moduleJson = await fetch("https://raw.githubusercontent.com/Bicycle-LEDs/electronics/main/" + modules[k].path + "/main.json").then(res => res.json())
-      
+      if(selectedSubMenu == -1 && selectedMenu == k) menu.innerHTML = menu.innerHTML + "\n" + '<ul onclick="mainDoc(' + k + ')" class="selected" id="' + modules[k].name + '">' + modules[k].name
+      else menu.innerHTML = menu.innerHTML + "\n" + '<ul onclick="mainDoc(' + k + ')" id="' + modules[k].name + '">' + modules[k].name
       let moduleMenu = document.getElementById(modules[k].name)
+
       for (let i = 0; i < moduleJson.components.length; i++) {
-        moduleMenu.innerHTML = moduleMenu.innerHTML + "\n" + '<li class="sub" onclick="subDoc('+ k + ', ' + i + ')">' + moduleJson.components[i].menuname + "</li>"
+        if(selectedMenu == k && selectedSubMenu == i) moduleMenu.innerHTML = moduleMenu.innerHTML + "\n" + '<li class="selected" onclick="subDoc('+ k + ', ' + i + ')">' + moduleJson.components[i].menuname + "</li>"
+        else moduleMenu.innerHTML = moduleMenu.innerHTML + "\n" + '<li onclick="subDoc('+ k + ', ' + i + ')">' + moduleJson.components[i].menuname + "</li>"
       }
 
       menu.innerHTML = menu.innerHTML + "</ul>"
     }
   }
+
   catch(err) {
-    var loading = document.getElementById("loading")
-    loading.classList.add("e")
+    if(firstRun) loading.classList.add("e")
+  }
+
+  if(!firstRun) {
+    setTimeout(() => {
+      loading.style.display = "none"
+      main.style.display = "block"
+    }, 700);
   }
 }
 
 buildMenu()
 
-
 async function mainDoc(moduleNum) {
-
+  if(subClicked) return
+  selectedMenu = moduleNum
+  selectedSubMenu = -1
+  buildMenu(false)
 }
 
-async function subDock(moduleNum, componentNum) {
-  
+async function subDoc(moduleNum, componentNum) {
+  subClicked = true
+  selectedMenu = moduleNum
+  selectedSubMenu = componentNum
+  await buildMenu(false)
+  subClicked = false
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,51 @@
+// <-- Loading animation -->
+
+var loading = document.getElementById("loading")
+var main = document.getElementById("mainSection")
+var loadingAnim = true
+
+loading.innerHTML = '<i class="fa-solid fa-spinner" id="emojiInLoading"></i> Loading'
+
+if(loading.classList.contains("mainPageLoading")) {
+  loading.style.lineHeight = "80vh"
+  loading.style.height = "100vh"
+  loading.style.animationName = "mainPageLoading"
+}
+
+function error() {
+  setTimeout(() => {
+    loading.style.animationDelay = "0s"
+    loading.style.animation = "loadingError 2s infinite"
+    loading.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> Error'
+    var announcment = document.getElementById("announcment")
+    announcment.innerHTML = announcment.innerHTML + '<br />' + '<i class="fa-solid fa-bug"></i> Loading errors occured'
+    var title = document.getElementById("title")
+    title.style.animation = "loadingError 2s infinite"
+    setTimeout(() => {
+      loading.style.display = "none"
+      main.style.display = "block"
+      loading.style.animation = "loading 2s infinite"
+      loading.innerHTML = '<i class="fa-solid fa-spinner" id="emojiInLoading"></i> Loading'
+      var emojiInLoading = document.getElementById("emojiInLoading")
+      emojiInLoading.style.animationDelay = "0s"
+    }, loadingAnim ? 2500 : 1);
+  }, loadingAnim ? 2000 : 1)
+}
+
+window.onload = function() {
+  setTimeout(() => {
+    var emojiInLoading = document.getElementById("emojiInLoading")
+    emojiInLoading.style.animationDelay = "0s"
+    if(loading.classList.contains("e")) {
+      error()
+      return
+    }
+    loading.style.display = "none"
+    main.style.display = "block"
+  }, loadingAnim ? 1000 : 1);
+}
+
+
 // <-- Announcment -->
 var globalAnnouncment = ''
 
@@ -7,18 +55,12 @@ var mainAnnouncment = '',
 
 var fileName = location.href.split("/").slice(-1), text;
 
-setInterval(() => {
-  var announcment = document.getElementById("announcment")
-  var today = new Date();
-  var time = today.getHours() + ":" + (today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes()) + ":" + (today.getSeconds() < 9 ? "0" + today.getSeconds() : today.getSeconds());
-  timeAnnouncment = globalAnnouncment ? globalAnnouncment + " | " + time : time
-  if(fileName[0] == 'docs.html') text = docsAnnouncment
-  else if (fileName[0] == 'about.html') text = aboutAnnouncment
-  else if (fileName[0] == 'index.html') text = mainAnnouncment
-  text ? (timeAnnouncment ? text = text + '<br />' + timeAnnouncment : text = text) : (timeAnnouncment ? text = timeAnnouncment : text = '')
-  textToDeliver = announcment.classList.contains("e") ? text + '<br />' + '<i class="fa-solid fa-bug"></i> Loading errors occured' : text
-  announcment.innerHTML =  textToDeliver
-}, 700)
+var announcment = document.getElementById("announcment")
+if(fileName[0] == 'docs.html') text = docsAnnouncment
+else if (fileName[0] == 'about.html') text = aboutAnnouncment
+else if (fileName[0] == 'index.html') text = mainAnnouncment
+text ? (globalAnnouncment ? text = text + '<br />' + globalAnnouncment : text = text) : (globalAnnouncment ? text = globalAnnouncment : text = '')
+announcment.innerHTML =  text
 
 
 // <-- Navigator -->
@@ -31,45 +73,8 @@ function hideMenu() {
   navLinks.style.right = "-140px"
 }
 
+// <-- Reloader -->
+
 function reload() {
   location.reload(true);
-}
-
-
-// <-- Loading animation -->
-
-var loading = document.getElementById("loading")
-var main = document.getElementById("mainSection")
-
-if(loading.classList.contains("mainPageLoading")) {
-  loading.style.lineHeight = "80vh"
-  loading.style.height = "100vh"
-  loading.style.animationName = "mainPageLoading"
-}
-
-function error() {
-  setTimeout(() => {
-    loading.style.animation = "loadingError 2s infinite"
-    loading.style.animationDelay = "0s"
-    loading.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> Error'
-    var announcment = document.getElementById("announcment")
-    announcment.classList.add("e")
-    var title = document.getElementById("title")
-    title.style.animation = "loadingError 2s infinite"
-    setTimeout(() => {
-      loading.style.display = "none"
-      main.style.display = "block"
-    }, 2500);
-  }, 2000)
-}
-
-window.onload = function() {
-  setTimeout(() => {
-    if(loading.classList.contains("e")) {
-      error()
-      return
-    }
-    loading.style.display = "none"
-    main.style.display = "block"
-  }, 1000);
 }
